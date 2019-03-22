d3470a158503836794cf2ef80fdb83969dd094e9
zf <820674670@qq.com>
2019-03-22 09:47:05 +0800

    Initial commit

---
+++ src/main/java/com/study/concurrency/concurrencydemo/ConcurrencydemoApplication.java
@@ -0,0 +1,13 @@
+package com.study.concurrency.concurrencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencydemoApplication.class, args);
+	}
+
+}
